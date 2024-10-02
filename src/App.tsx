cb16c528a2f5cb984dfc45775583455004963070
Fresh-hongsi <qkrtlghd97@gmail.com>
2024-10-02 14:52:31 +0900

    CREATE: 상품 상세페이지 & 장바구니 관리

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MainContent from "./components/MainContent";
+import Cart from "./components/Cart";
+import ProductDetail from "./components/ProductDetail";
 import "./App.css";
 // 기타 필요한 컴포넌트 임포트
 
@@ -15,6 +17,8 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<MainContent />} />
         {/* 다른 페이지 라우트 설정 */}
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
       </Routes>
       <Footer />
     </Router>
